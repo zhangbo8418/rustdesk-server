2bc9d3b8fb60008a12e465cfa45606f3e146315e
Ronan <ronan.le_meillat@highcanfly.club>
2024-05-25 18:08:21 +0200

    1.1.99-28

--- build.rs
+++ build.rs
@@ -1,19 +1,19 @@
-use std::{env, fs::File, path::PathBuf};
+use std::{env, fs::File, io::Write, path::PathBuf};
 
 fn main() {
     hbb_common::gen_version();
-    // Récupère la version du crate principal
+    // Get the current version from the environment
     let version = env::var("CARGO_PKG_VERSION").unwrap();
 
-    // Construit le chemin du fichier dans le répertoire temporaire
+    // Build a temporary file path
     let tmp_dir = env::var("TMP").or_else(|_| env::var("TEMP")).or_else(|_| env::var("TMPDIR")).unwrap_or_else(|_| "/tmp".to_string());
     let mut path = PathBuf::from(tmp_dir);
-    path.push("version.txt");
+    path.push("version-8659B48F-5726-433D-BEC2-C7042FE9D93B.txt");
 
-    // Écrit la version dans le fichier
+    // write the version to the file
     let mut file = File::create(&path).unwrap();
-    writeln!(file, "{}", version).unwrap();
+    file.write_fmt(format_args!("{}", version)).unwrap();
 
-    // Passe la version à l'environnement de compilation
+    // Pass also the version to the build script
     println!("cargo:rustc-env=MAIN_PKG_VERSION={}", version);
 }

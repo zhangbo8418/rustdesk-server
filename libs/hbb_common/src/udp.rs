2707c333eb7e8090b3db89bd43d70ab3d088f152
裙下孤魂 <46797568+zhangbo8418@users.noreply.github.com>
2024-10-09 07:20:36 +0800

    Update udp.rs

--- libs/hbb_common/src/udp.rs
+++ libs/hbb_common/src/udp.rs
@@ -20,12 +20,13 @@ fn new_socket(addr: SocketAddr, reuse: bool, buf_size: usize) -> Result<Socket, 
         SocketAddr::V6(..) => Socket::new(Domain::ipv6(), Type::dgram(), None),
     }?;
     if reuse {
-        // windows has no reuse_port, but it's reuse_address
+        // windows has no reuse_port, but its reuse_address
         // almost equals to unix's reuse_port + reuse_address,
-        // though may introduce nondeterministic behavior
-        #[cfg(unix)]
-        socket.set_reuse_port(true).ok();
-        socket.set_reuse_address(true).ok();
+        // though may introduce nondeterministic behavior.
+        // illumos has no support for SO_REUSEPORT
+        #[cfg(all(unix, not(target_os = "illumos")))]
+        socket.set_reuse_port(true)?;
+        socket.set_reuse_address(true)?;
     }
     // only nonblocking work with tokio, https://stackoverflow.com/questions/64649405/receiver-on-tokiompscchannel-only-receives-messages-when-buffer-is-full
     socket.set_nonblocking(true)?;
